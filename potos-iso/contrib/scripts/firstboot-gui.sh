ad78c9023c08f5aac6255e4438dd7e24d2f698e3
Schroeffu <info@schroeffu.ch>
2022-11-23 11:42:25 +0100

    feat(boot): build iso, install iso, firstboot linked to Potos repos (#3)
    * init: initial commit
    
    * refactor(grub): modified install textline
    
    * fix(build): create necessary empty folder potos-images
    
    * docs(readme): shortened
    
    * docs(readme): shortened
    
    * docs(readme): shortened
    
    * fix: add yamllint config (#2)
    
    * feat: add container build pipeline (#3)
    
    * feat: add container build pipeline, run on each push
    
    * fix(ci): changed docker-compose.yml v3 to v3.2
    
    * fix(ci): changed docker-compose.yml v3 to v3.2 (#4)
    
    Co-authored-by: David Schroff <david.schroff@adfinis.com>
    
    * feat(semantic-releases): testing the semantic-releases feature
    
    * fix(yamllint): ignore nod_modules folder
    
    * feat(semantic-release): add develop branch for testing purposes
    
    * feat(semantic-release): add develop branch for testing purposes
    
    * Revert "feat(semantic-release): add develop branch for testing purposes"
    
    This reverts commit 38e58ce3a406a899f9d6e8fb59b1174272c98c21.
    
    * Revert "feat(semantic-release): add develop branch for testing purposes"
    
    This reverts commit 17d960d9d8872fcf56b377df498719feadd21696.
    
    * Revert "fix(yamllint): ignore nod_modules folder"
    
    This reverts commit c6dd6fdb204ee523a65271609fa6cd8f91b85afe.
    
    * Revert "feat(semantic-releases): testing the semantic-releases feature"
    
    This reverts commit 565cb8cc8cd1f0ecea98c47201050c445f679ed4.
    
    * feat(firstboot_vars): Make Docker ARGs .env and ENV potos_env available at Potos first boot.
    
    * fix(yamllint): trailing spaces
    
    * docs(readme): mention now the .env and potos_env
    
    * fix(env): default specs repo URL fixed
    
    * docs(readme): small adjustment
    
    * feat(env): merge two env-files from building (.env) and running (potos_env) into one file (.env), as we do not need two afaik.
    
    * feat(env): Make it possible to overwrite POTOS_CLIENT_NAME in .env to have Grub menuentry customized.
    
    * fix(env): add POTOS_CLIENT_NAME and POTOS_CLIENT_SHORTNAME into the ISO and firstboot.
    
    * docs(readme): reomve secondary unused env file
    
    * feat(keyboard_layout): add into firstboot, untested
    
    * Feat/firstboot script (#1)
    
    * feat(vars): git clone and run the specific specs repo according .env vars
    
    * refactor(env): rename disk encryption password var
    
    * fix(vars,env): fixed some vars and adjusted the env files.
    
    Co-authored-by: David Schroff <david.schroff@adfinis.com>
    
    * Feat/firstboot script (#2)
    
    * feat(vars): git clone and run the specific specs repo according .env vars
    
    * refactor(env): rename disk encryption password var
    
    * fix(vars,env): fixed some vars and adjusted the env files.
    
    * docs(readme): mentioned the old var, fixed
    
    Co-authored-by: David Schroff <david.schroff@adfinis.com>
    
    * docs(readme): adding .env var descriptions and defaults
    
    * docs(readme): short enhancement
    
    * fix(readme): syntax
    
    * fix(netplan): make the network manager available at firstboot and later
    
    * fix(env): minimal fix
    
    * fix(netplan): cloud-init stuck at boot, trying to fix
    
    * fix(netplan): add trailing space, exclude from yamllinting.
    
    * feature(install): this commit includes a running install process, but with dummy YAD prompts.
    
    Co-authored-by: David Schroff <david.schroff@adfinis.com>
    Co-authored-by: fadnincx <fadnincx@users.noreply.github.com>

---
+++ potos-iso/contrib/scripts/firstboot-gui.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Load all the environment variables
+source /setup/.env
+
+POTOS_VERSION="${POTOS_CLIENT_NAME} (${POTOS_CLIENT_SHORTNAME})"
+
+#/setup/change-keyboard-layout
+
+if [[ -f /setup/potos-version ]]; then
+  POTOS_VERSION="$(</setup/potos-version)"
+fi
+
+yad --fullscreen --title "${POTOS_CLIENT_NAME} Setup" \
+  --borders 20 --align center --button OK --image-on-top \
+  --image=/potos-setup/potos.png \
+  --text \
+"Welcome to the last step of the Potos installation
+
+Client: ${POTOS_VERSION}
+
+Environment Variables:
+POTOS_SPECS_REPOSITORY: ${POTOS_SPECS_REPOSITORY}
+POTOS_ADJOIN: ${POTOS_ADJOIN}
+POTOS_ENV: ${POTOS_ENV}
+POTOS_DISK_ENCRYPTION_INITIAL_PASSWORD: ${POTOS_DISK_ENCRYPTION_INITIAL_PASSWORD}
+POTOS_INITIAL_HOSTNAME: ${POTOS_INITIAL_HOSTNAME}
+POTOS_INITIAL_PASSWORD_HASH: ${POTOS_INITIAL_PASSWORD_HASH}
+POTOS_INITIAL_USERNAME: ${POTOS_INITIAL_USERNAME}
+POTOS_CLIENT_NAME: ${POTOS_CLIENT_NAME}
+POTOS_CLIENT_SHORTNAME: ${POTOS_CLIENT_SHORTNAME}
+
+POTOS_GIT_SPECS_URL: ${POTOS_GIT_SPECS_URL}
+POTOS_GIT_SPECS_REPO: ${POTOS_GIT_SPECS_REPO}
+POTOS_GIT_SPECS_BRANCH: ${POTOS_GIT_SPECS_BRANCH}
+
+
+Click OK to proceed the installation.
+"
+
+while [[ -z ${POTOS_USER} || -z ${POTOS_PASS} ]]; do
+  USERINPUT="$(yad --fullscreen --title '${POTOS_CLIENT_NAME} Setup' \
+    --borders 20 --align center \
+    --button gtk-ok --button "Change keyboard layout":"/setup/change-keyboard-layout" \
+    --image-on-top --image=/potos-setup/potos.png \
+    --text \
+"IMPORTANT: If your password contains special characters, please remember to select the appropriate keyboard layout first.
+
+Please enter your new credentials below 
+" \
+    --form \
+    --field 'Username' \
+    --field 'Password:H'
+  )"
+
+  POTOS_USER="$(echo ${USERINPUT} | cut -d '|' -f 1)"
+  POTOS_PASS="$(echo ${USERINPUT} | cut -d '|' -f 2)"
+
+  if [[ -z ${POTOS_USER} || -z ${POTOS_PASS} ]]; then
+    yad --title "${POTOS_CLIENT_NAME} Setup" \
+      --borders 20 --align center --button gtk-ok --image-on-top \
+      --image=/potos-setup/potos.png \
+      --text 'Please choose and enter your new credentials.'
+  fi
+done
+
+export POTOS_USER POTOS_PASS
+
+sudo mkdir -m 755 -p /var/log/${POTOS_CLIENT_SHORTNAME}
+sudo touch /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log
+sudo -E bash -c "/setup/finish.sh &> /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log" &
+
+FINISH_CMD_PID=${!}
+
+sudo chown gnome-initial-setup /dev/tty2
+
+sudo tail -f /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log | tee /dev/tty2 | yad --fullscreen --no-buttons --title "${POTOS_CLIENT_NAME} Setup" \
+  --progress --enable-log --log-expanded --log-on-top --log-height 500 \
+  --text 'Please wait until the setup is finished' &
+
+# Wait until the finish.sh background process exits and check its return code
+wait ${FINISH_CMD_PID}
+
+FINISH_CMD_RC=${?}
+
+if [[ ${FINISH_CMD_RC} -ne 0 ]]; then
+  case ${FINISH_CMD_RC} in
+    1) ERROR_MSG="General command failure" ;;
+    2) ERROR_MSG="Network connectivity issues" ;;
+    3) ERROR_MSG="AD join failure" ;;
+    4) ERROR_MSG="Ansible failure" ;;
+    5) ERROR_MSG="Vault failure" ;;
+    6) ERROR_MSG="ISO outdated" ;;
+    *) ERROR_MSG="Unknown error code ${FINISH_CMD_RC}" ;;
+  esac
+fi
+
+sudo cat /var/log/${POTOS_CLIENT_SHORTNAME}/setup.log | yad --fullscreen --title "${POTOS_CLIENT_NAME} Setup" \
+  --borders 20 --align center --button gtk-ok \
+  --button "Shutdown":"sudo systemctl halt" \
+  --text-info --tail \
+  --image=/setup/potos.png --image-on-top \
+  --text \
+"Installation failed
+
+The last step of the installation was not successful.
+  Error message: ${ERROR_MSG}
+  Return code: ${FINISH_CMD_RC}
+
+Click OK to retry or Shutdown to cancel.
+"
+
+sudo systemctl restart gdm.service
